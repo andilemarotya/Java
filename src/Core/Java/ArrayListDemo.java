bf1df6d67844c6691806d269f5b4df6ab5ef246a
Mandile Andile Marotya <marotyamandile@gmail.com>
2023-02-09 15:11:34 +0200

    Java files from my pc

---
+++ src/Core/Java/ArrayListDemo.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+    public static void main(String[] args){
+
+        List<Integer> arrayList = new ArrayList<Integer>(5);
+
+        for (int i=1 ; i<5 ; i++){
+            arrayList.add(i);
+        }
+        System.out.println(arrayList);
+
+        //Remove element at index 3
+        arrayList.remove(3);
+        System.out.println(arrayList);
+    }
+}
---
+++ src/Core/Java/BitwiseOperator.java
@@ -0,0 +1,15 @@
+public class BitwiseOperator {
+    public static void main(String[] args){
+        //Initial values
+        int a = 5;
+        int b = 7;
+
+        //bitwise and
+        // (0101 & 0111) = 0101 = 5
+        System.out.println("a&b= "+ (a & b));
+
+        //bitwise and
+        // (0101 & 0111) = 0111 = 7
+        System.out.println("a|b= "+ (a | b));
+    }
+}
